ff08249cc7cb6bab7d08047557365e9f68ca8fc2
sroccaserra <sroccaserra@yahoo.com>
2022-01-08 00:32:36 +0100

    ♻  Fix layouts indentation

--- moonlander/keymaps/sroccaserra/keymap.c
+++ moonlander/keymaps/sroccaserra/keymap.c
@@ -12,56 +12,79 @@ enum custom_keycodes {
   FR_RSPC,
 };
 
+#define HOME_Q MT(MOD_LCTL, FR_Q)
+#define HOME_S MT(MOD_LALT, FR_S)
+#define HOME_D MT(MOD_LGUI, FR_D)
+#define HOME_F MT(MOD_LSFT, FR_F)
+
+#define HOME_J MT(MOD_RSFT, FR_J)
+#define HOME_K MT(MOD_RGUI, FR_K)
+#define HOME_L MT(MOD_RALT, FR_L)
+#define HOME_M MT(MOD_RCTL, FR_M)
+
+#define HYPER_A LALT(LGUI(LCTL(LSFT(KC_A))))
+#define HYPER_S LALT(LGUI(LCTL(LSFT(KC_S))))
+#define HYPER_D LALT(LGUI(LCTL(LSFT(KC_D))))
+#define HYPER_F LALT(LGUI(LCTL(LSFT(KC_F))))
+#define HYPER_G LALT(LGUI(LCTL(LSFT(KC_G))))
+#define HYPER_V LALT(LGUI(LCTL(LSFT(KC_V))))
+
+#define HYPER_H LALT(LGUI(LCTL(LSFT(KC_H))))
+#define HYPER_J LALT(LGUI(LCTL(LSFT(KC_J))))
+#define HYPER_K LALT(LGUI(LCTL(LSFT(KC_K))))
+#define HYPER_L LALT(LGUI(LCTL(LSFT(KC_L))))
+#define HYPER_COMM LALT(LGUI(LCTL(LSFT(FR_COMM))))
+
 #include "keymap_combo.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, FR_A,           FR_Z,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
-    KC_BSPACE,      MT(MOD_LCTL, FR_Q),MT(MOD_LALT, KC_S),MT(MOD_LGUI, KC_D),MT(MOD_LSFT, KC_F),KC_G,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_H,           MT(MOD_RSFT, KC_J),MT(MOD_RGUI, KC_K),MT(MOD_RALT, KC_L),MT(MOD_RCTL, FR_M),KC_TRANSPARENT,
-    KC_TRANSPARENT, FR_W,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           FR_COMM,        FR_SCLN,        FR_COLN,        FR_EQL,         KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, MO(2),          MO(1),          KC_LGUI,                                                                                                        KC_TRANSPARENT, MO(3),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_SPACE,       KC_TAB,         KC_TRANSPARENT,                 KC_TRANSPARENT, KC_ESCAPE,      KC_ENTER
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, FR_A,    FR_Z,    KC_E,    KC_R,    KC_T,    KC_TRNS, /**/ KC_TRNS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_TRNS,
+    KC_BSPC, HOME_Q,  HOME_S,  HOME_D,  HOME_F,  KC_G,    KC_TRNS, /**/ KC_TRNS, KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_M,  KC_TRNS,
+    KC_TRNS, FR_W,    KC_X,    KC_C,    KC_V,    KC_B,             /**/          KC_N,    FR_COMM, FR_SCLN, FR_COLN, FR_EQL,  KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, MO(2),   MO(1),   KC_LGUI,          /**/          KC_TRNS, MO(3),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_SPC,  KC_TAB,  KC_TRNS,                                     /**/ KC_TRNS, KC_ESC,  KC_ENTER
   ),
   [1] = LAYOUT_moonlander(
-    RESET,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, FR_CIRC,        FR_EACU,        FR_DQUO,        FR_QUOT,        KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, FR_GRV,         FR_EGRV,        FR_UGRV,        FR_CCED,        FR_AGRV,        KC_TRANSPARENT,
-    KC_TRANSPARENT, FR_MINS,        FR_DLR,         FR_LPRN,        FR_RPRN,        KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, FR_HASH,        FR_LCBR,        FR_RCBR,        FR_ASTR,        FR_UNDS,        KC_TRANSPARENT,
-    KC_TRANSPARENT, FR_AT,          FR_AMPR,        FR_LABK,        FR_RABK,        KC_TRANSPARENT,                                 KC_TRANSPARENT, FR_LBRC,        FR_RBRC,        FR_PERC,        FR_EXLM,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, MO(4),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, FR_CIRC, FR_EACU, FR_DQUO, FR_QUOT, KC_TRNS, KC_TRNS, /**/ KC_TRNS, FR_GRV,  FR_EGRV, FR_UGRV, FR_CCED, FR_AGRV, KC_TRNS,
+    KC_TRNS, FR_MINS, FR_DLR,  FR_LPRN, FR_RPRN, KC_TRNS, KC_TRNS, /**/ KC_TRNS, FR_HASH, FR_LCBR, FR_RCBR, FR_ASTR, FR_UNDS, KC_TRNS,
+    KC_TRNS, FR_AT,   FR_AMPR, FR_LABK, FR_RABK, KC_TRNS,          /**/          KC_TRNS, FR_LBRC, FR_RBRC, FR_PERC, FR_EXLM, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          /**/          KC_TRNS, MO(4),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,                                     /**/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [2] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, ST_MACRO_0,     LALT(FR_DLR),   KC_TRANSPARENT, LGUI(LSFT(KC_5)),KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, ST_MACRO_1,     KC_TRANSPARENT, ST_MACRO_2,     ST_MACRO_3,     KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, FR_TILD,        FR_BSLS,        RALT(FR_MINS),  KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_BRIGHTNESS_DOWN,KC_BRIGHTNESS_UP,KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, LCTL(FR_MINS),  LCTL(FR_PLUS),  LCTL(FR_0),     KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,     KC_TRNS, KC_TRNS,         KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,   KC_TRNS,
+    KC_TRNS, KC_TRNS, ST_MACRO_0,LALT(FR_DLR),KC_TRNS, LGUI(LSFT(KC_5)),KC_TRNS, /**/ KC_TRNS, KC_TRNS, ST_MACRO_1,   KC_TRNS,      ST_MACRO_2,   ST_MACRO_3,KC_TRNS,
+    KC_TRNS, KC_MUTE, KC_VOLD,   KC_VOLU,     KC_TRNS, KC_TRNS,         KC_TRNS, /**/ KC_TRNS, KC_TRNS, FR_TILD,      FR_BSLS,      RALT(FR_MINS),KC_TRNS,   KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_BRID,   KC_BRIU,     KC_TRNS, KC_TRNS,                  /**/          KC_TRNS, LCTL(FR_MINS),LCTL(FR_PLUS),LCTL(FR_0),   KC_TRNS,   KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,     KC_TRNS, KC_TRNS,                  /**/          KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,   KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,                                                   /**/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [3] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, FR_1,           FR_2,           FR_3,           FR_4,           FR_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, FR_6,           FR_7,           FR_8,           FR_9,           FR_0,           KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_LCTRL,       KC_LALT,        KC_LGUI,        KC_LSHIFT,      KC_HOME,        KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       LCTL(RALT(KC_9)),KC_TRANSPARENT,
-    KC_TRANSPARENT, LCTL(KC_F4),    KC_DELETE,      LCTL(LSFT(KC_C)),LCTL(LSFT(KC_V)),LALT(KC_LEFT),                                  KC_TRANSPARENT, LGUI(KC_PGDOWN),LGUI(KC_PGUP),  LGUI(KC_HOME),  LALT(FR_SUP2),  KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, MO(4),          KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,         KC_TRNS,         KC_TRNS,      KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
+    KC_TRNS, FR_1,       FR_2,    FR_3,            FR_4,            FR_5,         KC_TRNS, /**/ KC_TRNS, FR_6,    FR_7,         FR_8,         FR_9,         FR_0,            KC_TRNS,
+    KC_TRNS, KC_LCTRL,   KC_LALT, KC_LGUI,         KC_LSHIFT,       KC_HOME,      KC_TRNS, /**/ KC_TRNS, KC_LEFT, KC_DOWN,      KC_UP,        KC_RIGHT,     LCTL(RALT(KC_9)),KC_TRNS,
+    KC_TRNS, LCTL(KC_F4),KC_DEL,  LCTL(LSFT(KC_C)),LCTL(LSFT(KC_V)),LALT(KC_LEFT),         /**/          KC_TRNS, LGUI(KC_PGDN),LGUI(KC_PGUP),LGUI(KC_HOME),LALT(FR_SUP2),   KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,         MO(4),           KC_TRNS,               /**/          KC_TRNS, KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,         KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_TRNS,                                                          /**/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [4] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 TO(5),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_TRANSPARENT,
-    KC_TRANSPARENT, LALT(LGUI(LCTL(LSFT(KC_A)))),LALT(LGUI(LCTL(LSFT(KC_S)))),LALT(LGUI(LCTL(LSFT(KC_D)))),LALT(LGUI(LCTL(LSFT(KC_F)))),LALT(LGUI(LCTL(LSFT(KC_G)))),KC_TRANSPARENT,                                                                 KC_TRANSPARENT, LALT(LGUI(LCTL(LSFT(KC_H)))),LALT(LGUI(LCTL(LSFT(KC_J)))),LALT(LGUI(LCTL(LSFT(KC_K)))),LALT(LGUI(LCTL(LSFT(KC_L)))),KC_F11,         KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LALT(LGUI(LCTL(LSFT(KC_V)))),KC_TRANSPARENT,                                 KC_TRANSPARENT, LALT(LGUI(LCTL(LSFT(FR_COMM)))),KC_TRANSPARENT, KC_TRANSPARENT, KC_F12,         KC_TRANSPARENT,
-    KC_TRANSPARENT, WEBUSB_PAIR,    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /**/ TO(5),   KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_F1,      KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS, /**/ KC_TRNS, KC_F6,   KC_F7,     KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+    KC_TRNS, HYPER_A,    HYPER_S, HYPER_D, HYPER_F, HYPER_G, KC_TRNS, /**/ KC_TRNS, HYPER_H, HYPER_J,   HYPER_K, HYPER_L, KC_F11,  KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, HYPER_V, KC_TRNS,          /**/          KC_TRNS, HYPER_COMM,KC_TRNS, KC_TRNS, KC_F12,  KC_TRNS,
+    KC_TRNS, WEBUSB_PAIR,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          /**/          KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_TRNS,                                     /**/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [5] = LAYOUT_moonlander(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 TO(0),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, LALT(KC_F12),   KC_F12,
-    KC_TRANSPARENT, KC_ESCAPE,      FR_A,           FR_Z,           KC_E,           KC_R,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_T,           KC_Y,           KC_UP,          KC_I,           KC_O,           KC_P,
-    KC_TRANSPARENT, KC_LCTRL,       FR_Q,           KC_S,           KC_D,           KC_F,           KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_G,           KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_L,           KC_SCOLON,
-    KC_TRANSPARENT, KC_LSHIFT,      FR_W,           KC_X,           KC_C,           KC_V,                                           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_RALT,        KC_SLASH,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, /**/ TO(0),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LALT(KC_F12),KC_F12,
+    KC_TRNS, KC_ESC,  FR_A,    FR_Z,    KC_E,    KC_R,    KC_TRNS, /**/ KC_TRNS, KC_T,    KC_Y,    KC_UP,   KC_I,    KC_O,        KC_P,
+    KC_TRNS, KC_LCTL, FR_Q,    KC_S,    KC_D,    KC_F,    KC_TRNS, /**/ KC_TRNS, KC_G,    KC_LEFT, KC_DOWN, KC_RIGHT,KC_L,        KC_SCOLON,
+    KC_TRNS, KC_LSFT, FR_W,    KC_X,    KC_C,    KC_V,             /**/          KC_B,    KC_N,    KC_M,    KC_COMMA,KC_RALT,     KC_SLASH,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          /**/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS,                                     /**/ KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 
