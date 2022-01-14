sroccaserra/keyboard

commit d62ff003d7bea67d54cd00b960efe36f050b336a

♻  Move defines to keymap.h

moonlander/keymaps/sroccaserra/keymap.c
@@ -2,6 +2,9 @@
 #include "version.h"
 #include "keymap_french.h"
 
+#include "keymap.h"
+#include "keymap_combo.h"
+
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
   E_A_MAJ, // É
@@ -11,46 +14,6 @@ enum custom_keycodes {
   FR_LSPO,
   FR_RSPC,
 };
-
-#define HOME_Q MT(MOD_LCTL, FR_Q)
-#define HOME_S MT(MOD_LALT, FR_S)
-#define HOME_D MT(MOD_LGUI, FR_D)
-#define HOME_F MT(MOD_LSFT, FR_F)
-
-#define HOME_J MT(MOD_RSFT, FR_J)
-#define HOME_K MT(MOD_RGUI, FR_K)
-#define HOME_L MT(MOD_RALT, FR_L)
-#define HOME_M MT(MOD_RCTL, FR_M)
-
-#define CTL_PLU LCTL(FR_PLUS)
-#define CTL_MIN LCTL(FR_MINS)
-#define CTL_ZER LCTL(FR_0)
-#define _PIPE   RALT(FR_MINS)
-
-#define SH_COPY LCTL(LSFT(KC_C))
-#define SH_PAST LCTL(LSFT(KC_V))
-#define WO_LEFT LALT(KC_LEFT)
-#define TAB_CLO LCTL(KC_F4)
-#define SC_DOWN LGUI(KC_PGDN)
-#define SC_UP   LGUI(KC_PGUP)
-#define SC_HOME LGUI(KC_HOME)
-#define WI_NEXT LALT(FR_SUP2)
-#define VIM_ALT LCTL(RALT(KC_9))
-
-#define HYPER_A LALT(LGUI(LCTL(LSFT(KC_A))))
-#define HYPER_S LALT(LGUI(LCTL(LSFT(KC_S))))
-#define HYPER_D LALT(LGUI(LCTL(LSFT(KC_D))))
-#define HYPER_F LALT(LGUI(LCTL(LSFT(KC_F))))
-#define HYPER_G LALT(LGUI(LCTL(LSFT(KC_G))))
-#define HYPER_V LALT(LGUI(LCTL(LSFT(KC_V))))
-
-#define HYPER_H LALT(LGUI(LCTL(LSFT(KC_H))))
-#define HYPER_J LALT(LGUI(LCTL(LSFT(KC_J))))
-#define HYPER_K LALT(LGUI(LCTL(LSFT(KC_K))))
-#define HYPER_L LALT(LGUI(LCTL(LSFT(KC_L))))
-#define HYPER_COMM LALT(LGUI(LCTL(LSFT(FR_COMM))))
-
-#include "keymap_combo.h"
 
 enum layers {
     _AZERTY = 0,

moonlander/keymaps/sroccaserra/keymap.h
@@ -1,0 +1,44 @@
+#ifndef __KEYMAP_H__
+#define __KEYMAP_H__
+
+#include "keymap_french.h"
+
+#define HOME_Q MT(MOD_LCTL, FR_Q)
+#define HOME_S MT(MOD_LALT, FR_S)
+#define HOME_D MT(MOD_LGUI, FR_D)
+#define HOME_F MT(MOD_LSFT, FR_F)
+
+#define HOME_J MT(MOD_RSFT, FR_J)
+#define HOME_K MT(MOD_RGUI, FR_K)
+#define HOME_L MT(MOD_RALT, FR_L)
+#define HOME_M MT(MOD_RCTL, FR_M)
+
+#define CTL_PLU LCTL(FR_PLUS)
+#define CTL_MIN LCTL(FR_MINS)
+#define CTL_ZER LCTL(FR_0)
+#define _PIPE   RALT(FR_MINS)
+
+#define SH_COPY LCTL(LSFT(KC_C))
+#define SH_PAST LCTL(LSFT(KC_V))
+#define WO_LEFT LALT(KC_LEFT)
+#define TAB_CLO LCTL(KC_F4)
+#define SC_DOWN LGUI(KC_PGDN)
+#define SC_UP   LGUI(KC_PGUP)
+#define SC_HOME LGUI(KC_HOME)
+#define WI_NEXT LALT(FR_SUP2)
+#define VIM_ALT LCTL(RALT(KC_9))
+
+#define HYPER_A LALT(LGUI(LCTL(LSFT(KC_A))))
+#define HYPER_S LALT(LGUI(LCTL(LSFT(KC_S))))
+#define HYPER_D LALT(LGUI(LCTL(LSFT(KC_D))))
+#define HYPER_F LALT(LGUI(LCTL(LSFT(KC_F))))
+#define HYPER_G LALT(LGUI(LCTL(LSFT(KC_G))))
+#define HYPER_V LALT(LGUI(LCTL(LSFT(KC_V))))
+
+#define HYPER_H LALT(LGUI(LCTL(LSFT(KC_H))))
+#define HYPER_J LALT(LGUI(LCTL(LSFT(KC_J))))
+#define HYPER_K LALT(LGUI(LCTL(LSFT(KC_K))))
+#define HYPER_L LALT(LGUI(LCTL(LSFT(KC_L))))
+#define HYPER_COMM LALT(LGUI(LCTL(LSFT(FR_COMM))))
+
+#endif // __KEYMAP_H__

commit fb1a0834da8b09715fe888301e7e7533de4a80e4

🚑 Fix build, broken since d62ff003d7bea67d5
Not detected because the keymap.h name conflict broke the build of a qmk
c file that didn't need to be rebuilt since that time. A `male clean`
revealed it, a git bisect run found the problem without too much effort.

moonlander/keymaps/sroccaserra/keymap.c
@@ -2,8 +2,8 @@
 #include "version.h"
 #include "keymap_french.h"
 
-#include "keymap.h"
-#include "keymap_combo.h"
+#include "./keymap_defines.h"
+#include "./keymap_combo.h"
 
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,

moonlander/keymaps/sroccaserra/keymap_defines.h
@@ -1,5 +1,5 @@
-#ifndef __KEYMAP_H__
-#define __KEYMAP_H__
+#ifndef __KEYMAP_DEFINES_H__
+#define __KEYMAP_DEFINES_H__
 
 #include "keymap_french.h"
 
@@ -47,4 +47,4 @@
 
 #define _MISTER LALT(KC_F12)
 
-#endif // __KEYMAP_H__
+#endif // __KEYMAP_DEFINES_H__