af640317fb649e561618a8ab7eefd699c68947f0
sroccaserra <sroccaserra@yahoo.com>
2022-10-10 09:41:27 +0200

    Restore shift, enter, esc, tab, bspc keys

--- moonlander/keymaps/sroccaserra/keymap.c
+++ moonlander/keymaps/sroccaserra/keymap.c
@@ -28,11 +28,11 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_AZERTY] = LAYOUT_moonlander(
     _______, _______, _______, _______, _______, _______, _______,      TO(5)  , _______, _______, _______, _______, _______, _______,
-    _______, FR_A,    FR_Z,    KC_E,    KC_R,    KC_T,    _______,      _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
-    _______, HOME_Q,  HOME_S,  HOME_D,  HOME_F,  KC_G,    _______,      _______, KC_H,    HOME_J,  HOME_K,  HOME_L,  HOME_M,  _______,
-    _______, FR_W,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    FR_COMM, FR_SCLN, FR_COLN, FR_EQL,  _______,
+    KC_ESC,  FR_A,    FR_Z,    KC_E,    KC_R,    KC_T,    _______,      _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  HOME_Q,  HOME_S,  HOME_D,  KC_F,    KC_G,    _______,      _______, KC_H,    KC_J,    HOME_K,  HOME_L,  HOME_M,  _______,
+    KC_LSFT, FR_W,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    FR_COMM, FR_SCLN, FR_COLN, FR_EQL,  KC_RSFT,
     _______, _______, _______, MO(2),   MO(1),            _______,      _______,          MO(3),   MO(6),   _______, _______, _______,
-                                        KC_SPC,  _______, _______,      _______, KC_LGUI, CAPSWRD
+                                        KC_SPC,  _______, _______,      _______, KC_LGUI, KC_ENT
   ),
   [_SYMBOLS] = LAYOUT_moonlander(
     RESET,   _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______,
